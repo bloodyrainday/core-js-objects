917aa1a2743ce01c69190014604e5c5b7570de41
Mikita Bialko <bloodyrainday@gmail.com>
2025-12-06 15:53:43 +0300

    feat: solve makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -144,8 +144,19 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  let sumOfValues = 0;
+  Object.keys(lettersObject).forEach((key) => {
+    sumOfValues += lettersObject[key].length;
+  });
+  const res = Array(sumOfValues);
+  Object.entries(lettersObject).forEach((obj) => {
+    const [key, arrOfValues] = obj;
+    arrOfValues.forEach((val) => {
+      res.splice(val, 1, key);
+    });
+  });
+  return res.join('');
 }
 
 /**
