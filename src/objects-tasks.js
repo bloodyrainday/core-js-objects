920604597813696e220717193a5b627d10278455
Mikita Bialko <bloodyrainday@gmail.com>
2025-12-06 18:12:05 +0300

    feat: solve fromJSON task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -233,8 +233,14 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = Object.create(proto);
+  const parse = JSON.parse(json);
+  Object.entries(parse).forEach((obj) => {
+    const [key, value] = obj;
+    res[key] = value;
+  });
+  return res;
 }
 
 /**
