1151dfad92cf8d3e24e2de431c9aeef636026f7d
Mikita Bialko <bloodyrainday@gmail.com>
2025-12-06 11:10:18 +0300

    feat: solve mergeObjects task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -32,8 +32,20 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  if (objects.length === 0) return {};
+  const res = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach((el) => {
+      const [key, val] = el;
+      if (res[key]) {
+        res[key] += val;
+      } else {
+        res[key] = val;
+      }
+    });
+  });
+  return res;
 }
 
 /**
