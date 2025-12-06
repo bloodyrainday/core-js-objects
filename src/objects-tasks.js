ff0efc6266749aee56aa7890fd03dcc12621fc0f
Mikita Bialko <bloodyrainday@gmail.com>
2025-12-06 11:17:23 +0300

    feat: solve removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -61,8 +61,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const objCopy = { ...obj };
+  keys.forEach((key) => {
+    if (objCopy[key]) {
+      delete objCopy[key];
+    }
+  });
+  return objCopy;
 }
 
 /**
