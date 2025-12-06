eaae52ee9cffee0d1edaf3c4af8309561d12c4f0
Mikita Bialko <bloodyrainday@gmail.com>
2025-12-06 16:37:34 +0300

    fix: fix the warning of Rectangle task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -203,7 +203,7 @@ function sellTickets(queue) {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
+  this.getArea = function culcArea() {
     return this.width * this.height;
   };
 }
